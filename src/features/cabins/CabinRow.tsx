588677a229607858bef6c7666798ce61fa52804b
behzad aslani <behzadaslani80@gmail.com>
2024-12-11 13:24:00 +0330

    feat: implemented quering and mutating cabins data

--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import { formatCurrency } from '../../utils/helpers'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { deleteCabin } from '../../services/apiCabins'
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +41,51 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `
+
+type Props = {
+  cabin: {
+    id: number
+    name: string
+    maxCapacity: number
+    regularPrice: number
+    discount: number
+    image: string
+  }
+}
+
+export default function CabinRow({ cabin }: Props) {
+  const {
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+    id: cabinId,
+  } = cabin
+
+  const queryClient = useQueryClient()
+  const { isPending, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['cabins'],
+      })
+    },
+    onError: (err) => console.error(err.message),
+  })
+
+  return (
+    <>
+      <TableRow role='row'>
+        <Img src={image} />
+        <Cabin>{name}</Cabin>
+        <div>Fits up to {maxCapacity} guests</div>
+        <Price>{formatCurrency(regularPrice)}</Price>
+        <Discount>{formatCurrency(discount)}</Discount>
+        <button onClick={() => mutate(cabinId)} disabled={isPending}>
+          Delete
+        </button>
+      </TableRow>
+    </>
+  )
+}
--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,16 +1,18 @@
+import CabinTable from '../features/cabins/CabinTable'
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
 
 function Cabins() {
   return (
-    <Row typeof='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src='https://bshphhapjeykudbfkcwh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg'
-        alt=''
-      />
-    </Row>
+    <>
+      <Row typeof='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   )
 }
 
