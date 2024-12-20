e067e4c8aba6d7dcdd5898d5815f072fa8c357b9
behzad aslani <behzadaslani80@gmail.com>
2024-12-20 13:11:31 +0330

    feat: implemented dark mode

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,45 @@
+import { createContext, ReactNode, useContext, useEffect } from 'react'
+import { useLocalStorageState } from '../hooks/useLocalStorageState'
+
+const DarkModeContext = createContext({
+  isDarkMode: false,
+  toggleDarkMode: () => {},
+})
+
+function DarkModeProvider({ children }: { children: ReactNode }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode')
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode')
+      document.documentElement.classList.remove('light-mode')
+    } else {
+      document.documentElement.classList.add('light-mode')
+      document.documentElement.classList.remove('dark-mode')
+    }
+  }, [isDarkMode])
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark: boolean) => !isDark)
+  }
+  return (
+    <DarkModeContext.Provider
+      value={{
+        isDarkMode,
+        toggleDarkMode,
+      }}
+    >
+      {children}
+    </DarkModeContext.Provider>
+  )
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext)
+  if (context === undefined)
+    throw new Error('DarkModeContext was used outside of dark mode provider')
+  return context
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { DarkModeProvider, useDarkMode }
