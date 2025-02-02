45196e326615733b453e6ca9fac62490225835bb
behzad aslani <behzadaslani80@gmail.com>
2025-02-02 22:59:32 +0330

    chore: added filled credential for presentation

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin'
 import SpinnerMini from '../../ui/Spinner.Mini'
 
 function LoginForm() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('behzadaslani@gmail.com')
+  const [password, setPassword] = useState('Behzad@admin')
   const { login, isLogingIn } = useLogin()
 
   function handleSubmit(e: FormEvent) {
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,6 @@ import Heading from '../ui/Heading'
 const LoginLayout = styled.main`
   min-height: 100vh;
   display: grid;
-  grid-template-columns: 48rem;
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
